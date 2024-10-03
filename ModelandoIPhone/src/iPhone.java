8f10aa687e3608bbee44b4b0ad9ca5c59ebe17c8
Daniel <daniel10gja@gmail.com>
2024-10-03 10:31:24 -0300

    Create iPhone.java

---
+++ ModelandoIPhone/src/iPhone.java
@@ -0,0 +1,46 @@
+public class iPhone extends AparelhoTelefonico implements ReprodutorMusical, NavegadorInternet {
+
+    private String modelo;
+
+    public void ligar(String numero) {
+
+    }
+
+    public void atender() {
+
+    }
+
+    public void iniciarCorreioVoz() {
+    }
+
+    @Override
+    public void tocar() {
+        System.out.println("Reproduzindo música...");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Música selecionada: " + musica);
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada.");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Página atualizada.");
+    }
+
+}
