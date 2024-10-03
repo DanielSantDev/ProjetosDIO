d435c55879460bbe512f232cef890166ce9a1fc6
Daniel <daniel10gja@gmail.com>
2024-10-03 10:31:19 -0300

    Update AparelhoTelefonico.java

--- ModelandoIPhone/src/AparelhoTelefonico.java
+++ ModelandoIPhone/src/AparelhoTelefonico.java
@@ -1,15 +1,15 @@
 public class AparelhoTelefonico {
 
-    public void ligar() {
-
+    public void ligar(String numero) {
+        System.out.println("Ligando para: " + numero);
     }
 
-    public void atender () {
-
+    public void atender() {
+        System.out.println("Atendendo a chamada.");
     }
 
     public void iniciarCorreioVoz() {
-
+        System.out.println("Iniciando correio de voz.");
     }
 
 }
